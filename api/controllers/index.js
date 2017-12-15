6a3bde52e05ff7acf4bd178bf2137916f96a30dd
Ryan Hamblin <ryanhca@users.noreply.github.com>
2017-12-15 00:18:53 -0700

    Update index.js

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,6 +1,6 @@
-const jokes = require('./jokes');
-const login = require('./login');
-const createUser = require('./user');
+const { jokes } = require('./jokes');
+const { login } = require('./login');
+const { createUser } = require('./user');
 
 module.exports = {
   jokes,

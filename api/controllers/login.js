bd20eb165e14e987e79595630730d3686249c311
Ryan Hamblin <ryanhca@users.noreply.github.com>
2017-12-15 00:35:40 -0700

    Update login.js

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -10,9 +10,7 @@ const login = (req, res) => {
   const payload = {
     username: req.username
   }; // what will determine our payload.
-  const token = jwt.sign(payload, mysecret, {
-    expiresInMinutes: 1440 // expires in 24 hours
-  }); // creates our JWT with a secret and a payload and a hash.
+  const token = jwt.sign(payload, mysecret); // creates our JWT with a secret and a payload and a hash.
   res.json({ token }); // sends the token back to the client
 };
 

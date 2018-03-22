8195a9a8c88fe54486f5f5794ae3d7dc9060a574
Ryan Hamblin <ryanhca@gmail.com>
2018-03-22 16:02:14 -0600

    solution

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,15 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  const myUser = new User({ username, password });
+  myUser
+    .save()
+    .then(user => {
+      res.json(user);
+    })
+    .catch(err => {
+      return res.status(500).json({ error: err });
+    });
 };
 
 module.exports = {
--- api/models/userModels.js
+++ api/models/userModels.js
@@ -8,6 +8,16 @@ const UserSchema = Schema({
   // create your user schema here.
   // username: required, unique and lowercase
   // password: required
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 UserSchema.pre('save', function(next) {
@@ -15,13 +25,22 @@ UserSchema.pre('save', function(next) {
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
+  bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+    if (err) next(err);
+    this.password = hash;
+    next();
+  });
 });
 
-UserSchema.methods.checkPassword = function() {
+UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  bcrypt.compare(plainTextPW, this.password, (err, isMatch) => {
+    if (err) return callBack(err);
+    callBack(null, isMatch);
+  });
 };
 
 // if you're really stuck with this at this point, you can reference this document.

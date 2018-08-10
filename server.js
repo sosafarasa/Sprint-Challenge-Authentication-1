c3ec0e65e7b977044b821040cef8266ea16f2b6a
Luis Hernandez <luishrd@gmail.com>
2018-08-09 18:48:31 -0600

    updates instructions

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const routes = require('./api/routes/routes');
@@ -11,11 +10,11 @@ const corsOptions = {
   // then pass this object to the cors() function
 };
 
-server.use(bodyParser.json());
+server.use(express.json());
 server.use(cors());
 
 routes(server);
 
 module.exports = {
-  server
+  server,
 };
